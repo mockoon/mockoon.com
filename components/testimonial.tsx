a0958ba94a15ba3353c817efb5c8ce24dcc3408f
Guillaume <hi@255kb.dev>
2020-10-20 08:51:51 +0200

    Update sitemap

--- components/testimonial.tsx
+++ components/testimonial.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from 'react';
+import { Fragment, FunctionComponent } from 'react';
 
 const Testimonial: FunctionComponent<{
   link: string;
@@ -7,29 +7,32 @@ const Testimonial: FunctionComponent<{
   username: string;
 }> = function (props) {
   return (
-    <div className='card'>
-      <div className='card-content'>
-        <div className='content'>{props.children}</div>
-        <a href={props.link} rel='noopener' target='_blank'>
-          <div className='media'>
-            <div className='media-left'>
-              <figure className='image is-32x32'>
-                <img
-                  className='is-rounded'
-                  src={props.imgSrc}
-                  alt={props.username + ' profile picture'}
-                  loading='lazy'
-                />
-              </figure>
+    <Fragment>
+      <style>{`.card {height:100%}`}</style>
+      <div className='card'>
+        <div className='card-content'>
+          <div className='content'>{props.children}</div>
+          <a href={props.link} rel='noopener' target='_blank'>
+            <div className='media'>
+              <div className='media-left'>
+                <figure className='image is-32x32'>
+                  <img
+                    className='is-rounded'
+                    src={props.imgSrc}
+                    alt={props.username + ' profile picture'}
+                    loading='lazy'
+                  />
+                </figure>
+              </div>
+              <div className='media-content'>
+                <p className='title is-6'>{props.name}</p>
+                <p className='subtitle content is-small'>{props.username}</p>
+              </div>
             </div>
-            <div className='media-content'>
-              <p className='title is-6'>{props.name}</p>
-              <p className='subtitle content is-small'>{props.username}</p>
-            </div>
-          </div>
-        </a>
+          </a>
+        </div>
       </div>
-    </div>
+    </Fragment>
   );
 };
 

d99207ca8e2bdee6bb8f75e59a07c8775d3ecefb
Guillaume <hi@255kb.dev>
2021-05-31 10:34:48 +0200

    Make navbar collapsible

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,126 +1,143 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useState } from 'react';
 
 const Nav: FunctionComponent = function () {
   const router = useRouter();
-  return (
-    <div className='container'>
-      <nav className='navbar navbar-expand-lg navbar-light bg-white'>
-        <div className='container'>
-          <Link href='/'>
-            <a className='navbar-brand pt-0 mt-0 me-auto'>
-              <img
-                src='/images/logo.svg'
-                className='logo'
-                alt='Logo'
-                width='150'
-              />
-            </a>
-          </Link>
+  const [show, setShow] = useState(false);
 
-          <button
-            className='navbar-toggler'
-            type='button'
-            data-bs-toggle='collapse'
-            data-bs-target='#navbar'
-            aria-controls='navbar'
-            aria-expanded='false'
-            aria-label='Toggle navigation'
-          >
-            <span className='navbar-toggler-icon'></span>
-          </button>
-          <div
-            className='navbar-collapse d-flex justify-content-end collapse'
-            id='navbar'
-          >
-            <ul className='navbar-nav ms-auto'>
+  const toggler = (
+    <button
+      className='navbar-toggler'
+      type='button'
+      aria-controls='navbar'
+      aria-expanded='false'
+      aria-label='Toggle navigation'
+      onClick={() => setShow(!show)}
+    >
+      <span className='navbar-toggler-icon'></span>
+    </button>
+  );
+
+  return (
+    <nav className='navbar navbar-expand-lg navbar-light bg-white'>
+      <div className='container-fluid'>
+        <Link href='/'>
+          <a className='navbar-brand'>
+            <img
+              src='/images/logo.svg'
+              className='navbar-brand-img'
+              alt='Logo'
+            />
+          </a>
+        </Link>
+
+        {toggler}
+        <div
+          className={
+            show ? 'collapse navbar-collapse show' : 'collapse navbar-collapse'
+          }
+        >
+          <ul className='navbar-nav align-items-center ms-auto'>
+            {show && toggler}
+            {show && (
               <li className='nav-item'>
-                <Link href='/features/'>
+                <Link href='/'>
                   <a
                     className={`nav-link ${
-                      router.pathname === '/features' ? 'active' : ''
+                      router.pathname === '/' ? 'active' : ''
                     }`}
                   >
-                    Features
+                    Home
                   </a>
                 </Link>
               </li>
-              <li className='nav-item'>
-                <Link href='/docs/latest/about/'>
-                  <a
-                    className={`nav-link ${
-                      router.pathname === '/docs/[...slug]' ? 'active' : ''
-                    }`}
-                  >
-                    Docs
-                  </a>
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <Link href='/cli/'>
-                  <a
-                    className={`nav-link d-flex align-items-center ${
-                      router.pathname === '/cli' ? 'active' : ''
-                    }`}
-                  >
-                    <span className='badge bg-primary-soft'>New!</span>
-                    <span>&nbsp;CLI</span>
-                  </a>
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <Link href='/tutorials/'>
-                  <a
-                    className={`nav-link ${
-                      router.pathname === '/tutorials' ? 'active' : ''
-                    }`}
-                  >
-                    Tutorials
-                  </a>
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <Link href='/blog/'>
-                  <a
-                    className={`nav-link ${
-                      router.pathname === '/blog' ||
-                      router.pathname === '/blog/[slug]'
-                        ? 'active'
-                        : ''
-                    }`}
-                  >
-                    Blog
-                  </a>
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <Link href='/faq/'>
-                  <a
-                    className={`nav-link ${
-                      router.pathname === '/faq' ? 'active' : ''
-                    }`}
-                  >
-                    FAQ
-                  </a>
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <Link href='/sponsor-us/'>
-                  <a
-                    className={`nav-link ${
-                      router.pathname === '/sponsor-us' ? 'active' : ''
-                    }`}
-                  >
-                    Sponsor us
-                  </a>
-                </Link>
-              </li>
-            </ul>
-          </div>
+            )}
+            <li className='nav-item'>
+              <Link href='/features/'>
+                <a
+                  className={`nav-link ${
+                    router.pathname === '/features' ? 'active' : ''
+                  }`}
+                >
+                  Features
+                </a>
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link href='/docs/latest/about/'>
+                <a
+                  className={`nav-link ${
+                    router.pathname === '/docs/[...slug]' ? 'active' : ''
+                  }`}
+                >
+                  Docs
+                </a>
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link href='/cli/'>
+                <a
+                  className={`nav-link d-flex align-items-center ${
+                    router.pathname === '/cli' ? 'active' : ''
+                  }`}
+                >
+                  <span className='badge bg-primary-soft'>New!</span>
+                  <span>&nbsp;CLI</span>
+                </a>
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link href='/tutorials/'>
+                <a
+                  className={`nav-link ${
+                    router.pathname === '/tutorials' ? 'active' : ''
+                  }`}
+                >
+                  Tutorials
+                </a>
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link href='/blog/'>
+                <a
+                  className={`nav-link ${
+                    router.pathname === '/blog' ||
+                    router.pathname === '/blog/[slug]'
+                      ? 'active'
+                      : ''
+                  }`}
+                >
+                  Blog
+                </a>
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link href='/faq/'>
+                <a
+                  className={`nav-link ${
+                    router.pathname === '/faq' ? 'active' : ''
+                  }`}
+                >
+                  FAQ
+                </a>
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link href='/sponsor-us/'>
+                <a
+                  className={`nav-link ${
+                    router.pathname === '/sponsor-us' ? 'active' : ''
+                  }`}
+                >
+                  Sponsor us
+                </a>
+              </Link>
+            </li>
+          </ul>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 };
 

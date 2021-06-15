a9d88e6972926be1340cfbec0b647d549969bb4f
Andy Rasseneur <arasseneur@gmail.com>
2021-06-15 17:48:15 +0200

    code cleaning
    Code cleaning for simple cards

--- components/simple-cards.tsx
+++ components/simple-cards.tsx
@@ -6,9 +6,9 @@ const SimpleCards: FunctionComponent<{
   items: ItemCard;
 }> = function (props) {
   const numberOfColumns = 3;
-  const content = [];
+  const featuresContent = [];
   for (let rowIndex = 0; rowIndex < numberOfColumns; rowIndex++) {
-    content.push(
+    featuresContent.push(
       <div key={'featureRow' + rowIndex} className='col-md-4 py-3'>
         {props.items
           .filter((item, itemIndex) => {
@@ -52,53 +52,7 @@ const SimpleCards: FunctionComponent<{
     );
   }
 
-  const numberOfRows = Math.ceil(props.items.length / 3);
-  const featuresContent = [];
-
-  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
-    featuresContent.push(
-      <div key={'featureRow' + rowIndex} className='row'>
-        {props.items
-          .slice(rowIndex * 3, rowIndex * 3 + 3)
-          .map((item, itemIndex) => {
-            return (
-              <div className='col-md-4 py-3' key={'feature' + itemIndex}>
-                <div className='card card-border shadow-light-lg'>
-                  <div className='card-body text-center simple-card-min d-flex align-content-end flex-wrap justify-content-center '>
-                    <h4 className=''>{item.title}</h4>
-                    <p
-                      className='text-muted'
-                      dangerouslySetInnerHTML={{ __html: item.description }}
-                    ></p>
-                    {(item.link || (item.disabledLink && item.linkText)) && (
-                      <div
-                        className={`${item.disabledLink ? 'text-center' : ''}`}
-                      >
-                        <span>
-                          {!item.disabledLink && (
-                            <Link href={item.link}>
-                              <span className='btn btn-primary-soft btn-xs mb-0 mt-3 pb-0'>
-                                {item.linkText || 'Documentation'} →
-                              </span>
-                            </Link>
-                          )}
-                          {item.disabledLink && (
-                            <Fragment>{item.linkText}</Fragment>
-                          )}
-                        </span>
-                      </div>
-                    )}
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-      </div>
-    );
-  }
-
-  return <Fragment>{content}</Fragment>;
-  //return <Fragment>{featuresContent}</Fragment>;
+  return <Fragment>{featuresContent}</Fragment>;
 };
 
 export default SimpleCards;

9b8229b26eb7344b602edf75d2cb0c5de5d44627
Guillaume <guillaume@1kb.software>
2023-06-20 14:05:09 +0200

    Update video tutorials page

--- pages/video-tutorials.tsx
+++ pages/video-tutorials.tsx
@@ -1,4 +1,5 @@
-import Card from '../components/card';
+import Link from 'next/link';
+import { useState } from 'react';
 import Hero from '../components/hero';
 import Meta from '../components/meta';
 import Layout from '../layout/layout';
@@ -10,48 +11,93 @@ const meta = {
 };
 
 const VideoTutorials = function () {
+  const videos = [
+    {
+      id: 'XKMCKwxMkWs',
+      name: 'Getting started with Mockoon',
+      description:
+        'Mockoon is a free cross-platform desktop application that takes API mocking to the next level. Mockoon offers a fast and easy-to-use interface and gives you complete control over your mock APIs with advanced functionality like a templating system, a proxy mode, and requests recording. This tutorial will show you how to install the desktop mocking application and set up your first mock API.',
+      link: '/tutorials/getting-started/'
+    },
+    {
+      id: 'HDGeslYAEAc',
+      name: 'Responses and rules',
+      description:
+        'For each of Mockoon\'s API endpoint, you can define multiple responses with specific characteristics like status, body, and headers and serve them using a set of rules. In this tutorial, we will create a typical setup: one response returning a 200 status code and another one returning a 401 status code when the "Authorization" header is not present in the request.',
+      link: '/tutorials/responses-and-rules/'
+    },
+    {
+      id: 'UspMI8qLQUI',
+      name: 'Partial API mock with proxy mode',
+      description:
+        "During the development of an application, you may use an API that is still under development. Rather than waiting for new API endpoints to be added and rely on your memory or documentation to continue your application development, you could use Mockoon to create a partial fake API. The idea is simple. Mockoon would serve the API endpoints you define while forwarding anything else to the URL of your choice. Let's dive through this with a simple example.",
+      link: '/tutorials/partial-mocking-proxy/'
+    }
+  ];
+  const [activeVideo, setActiveVideo] = useState(videos[0]);
+
   return (
     <Layout footerBanner='download'>
       <Meta title={meta.title} description={meta.description} />
-      <Hero title={meta.title} subtitle={meta.description} />
+      <Hero
+        title={
+          '📽️ Learn with Mockoon\'s <span class="text-primary">video tutorials</span>'
+        }
+        subtitle={meta.description}
+      />
 
-      <section className='pb-8'>
+      <section className='py-6 py-md-8 border-top bg-gradient-light-white'>
         <div className='container'>
-          <div className='row d-flex flex-column flex-lg-row'>
-            <div className='mx-auto my-lg-3 col-12 col-xxl-10'>
-              <Card
-                data={{
-                  title: 'Getting started with Mockoon',
-                  description:
-                    'Learn how to create your first mock API with Mockoon.',
-                  imageSrc:
-                    'https://img.youtube.com/vi/XKMCKwxMkWs/maxresdefault.jpg',
-                  imageAlt: '',
-                  links: [
-                    {
-                      src: 'https://youtu.be/XKMCKwxMkWs',
-                      text: 'Watch on YouTube',
-                      icon: 'icon-youtube'
-                    }
-                  ]
-                }}
-              />
-              <Card
-                data={{
-                  title: 'Responses and rules',
-                  description:
-                    'Learn how to add more responses to your routes and serve them using the rules system.',
-                  imageSrc: `https://img.youtube.com/vi/HDGeslYAEAc/maxresdefault.jpg`,
-                  imageAlt: '',
-                  links: [
-                    {
-                      src: 'https://youtu.be/HDGeslYAEAc',
-                      text: 'Watch on YouTube',
-                      icon: 'icon-youtube'
-                    }
-                  ]
-                }}
-              />
+          <div className='row justify-content-center g-10'>
+            <div className='col-12 col-lg-3'>
+              <aside className='flex-grow-1 sticky-top pt-4'>
+                <ul className='card-list list mb-6'>
+                  <h6 className='fw-bold text-uppercase mb-2'>
+                    Tutorials list
+                  </h6>
+                  {videos.map((video, videoIndex) => {
+                    return (
+                      <li
+                        key={`template${videoIndex}`}
+                        className={`list-item py-1 ${
+                          activeVideo.id === video.id ? 'active' : ''
+                        }`}
+                      >
+                        <a
+                          href='#'
+                          className='list-link'
+                          onClick={(e) => {
+                            e.preventDefault();
+                            setActiveVideo(video);
+                          }}
+                        >
+                          {videoIndex + 1}. {video.name}
+                        </a>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </aside>
+            </div>
+            <div className='col-12 col-lg-9'>
+              <h2>{activeVideo.name}</h2>
+              <p className='mt-6'>{activeVideo.description}</p>
+              <div className='ratio ratio-16x9 mb-4'>
+                <iframe
+                  style={{ border: 'none' }}
+                  src={`https://www.youtube-nocookie.com/embed/${activeVideo.id}`}
+                  title='YouTube video player'
+                  allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+                  allowFullScreen={true}
+                ></iframe>
+              </div>
+
+              <p>
+                <Link href={activeVideo.link}>
+                  {/* with arrow */}
+                  <a className='btn btn-secondary-soft'>📃 Text version →</a>
+                </Link>
+              </p>
             </div>
           </div>
         </div>

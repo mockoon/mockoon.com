ff461745ea1ff40776ec1dc28fb33669d9366256
Guillaume <guillaume@mockoon.com>
2025-03-27 09:44:28 +0100

    Update favicon for dark mode

--- layout/layout.tsx
+++ layout/layout.tsx
@@ -16,25 +16,20 @@ const Layout: FunctionComponent<{
         <meta httpEquiv='X-UA-Compatible' content='IE=edge,chrome=1' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <link
+          rel='icon'
+          type='image/png'
+          href='/favicon-96x96.png'
+          sizes='96x96'
+        />
+        <link rel='icon' type='image/svg+xml' href='/favicon.svg' />
+        <link rel='shortcut icon' href='/favicon.ico' />
+        <link
           rel='apple-touch-icon'
           sizes='180x180'
           href='/apple-touch-icon.png'
         />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='32x32'
-          href='/favicon-32x32.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='16x16'
-          href='/favicon-16x16.png'
-        />
+        <meta name='apple-mobile-web-app-title' content='Mockoon' />
         <link rel='manifest' href='/site.webmanifest' />
-        <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#252830' />
-        <meta name='theme-color' content='#ffffff' />
       </Head>
       {!minimal && <Nav />}
 

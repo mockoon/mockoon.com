5b84c73d36a37d150ba607298519d01ec4f38c2e
Guillaume <guillaume@mockoon.com>
2025-09-22 10:48:19 +0200

    Update status

--- pages/status.tsx
+++ pages/status.tsx
@@ -18,7 +18,7 @@ const incidents: {
     desc: 'All services will be restarted for a routine maintenance. The downtime is expected to last less than 5 minutes for each service. We apologize for the inconvenience.',
     dateStart: '2025-09-20T06:30:00Z',
     dateEnd: '2025-09-20T07:00:00Z',
-    status: 'inprogress'
+    status: 'resolved'
   },
   {
     title: 'All services - Schedule downtime',

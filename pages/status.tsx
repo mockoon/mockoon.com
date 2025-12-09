04a95d0973e4bc692bf72e900ca928f8bfae650f
Guillaume <guillaume@mockoon.com>
2025-12-09 09:54:34 +0100

    Update status

--- pages/status.tsx
+++ pages/status.tsx
@@ -15,10 +15,10 @@ const incidents: {
 }[] = [
   {
     title: 'Cloud Deployments - US servers - Scheduled migration',
-    desc: 'Cloud deployments US servers will be migrated to a new infrastructure to improve performance, reliability and security. During this time, US customers instances will be temporarily unavailable. The migration is expected to last approximately 2 hours for all servers but individual instances downtime should not exceed 30 minutes. We apologize for the inconvenience.<br>Update (07:45 UTC): The migration of server "d1" has started.<br>Update (08:10 UTC): The migration of server "d1" is complete and instances are back online.<br>Update (08:20 UTC): The migration of server "us2" has started.',
+    desc: 'Cloud deployments US servers will be migrated to a new infrastructure to improve performance, reliability and security. During this time, US customers instances will be temporarily unavailable. The migration is expected to last approximately 2 hours for all servers but individual instances downtime should not exceed 30 minutes. We apologize for the inconvenience.<br>Update (07:45 UTC): The migration of server "d1" has started.<br>Update (08:10 UTC): The migration of server "d1" is complete and instances are back online.<br>Update (08:20 UTC): The migration of server "us2" has started.<br>Update (08:45 UTC): The migration of server "us2" is complete and instances are back online.<br>All US servers have been successfully migrated to the new infrastructure. We are monitoring the new setup closely to ensure everything is working as expected. We apologize for any inconvenience caused during this migration.',
     dateStart: '2025-12-09T07:00:00Z',
-    dateEnd: '2025-12-09T09:00:00Z',
-    status: 'inprogress'
+    dateEnd: '2025-12-09T08:55:00Z',
+    status: 'resolved'
   },
   {
     title: 'Incident - Unreachable Cloud instances',

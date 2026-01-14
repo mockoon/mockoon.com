54ceb2be67ec3eeb792a6f69ab5c15cf2a414810
Guillaume <guillaume@mockoon.com>
2026-01-14 09:33:26 +0100

    Update status

--- pages/status.tsx
+++ pages/status.tsx
@@ -17,8 +17,8 @@ const incidents: {
     title: 'All services - Scheduled downtime',
     desc: 'All services will be restarted for the deployment of v9.5.0. The downtime is expected to last less than 15 minutes for each service. We apologize for the inconvenience.',
     dateStart: '2026-01-14T07:20:00Z',
-    dateEnd: '2026-01-14T09:20:00Z',
-    status: 'inprogress'
+    dateEnd: '2026-01-14T08:30:00Z',
+    status: 'resolved'
   },
   {
     title: 'Cloud Deployments - US servers - Scheduled migration',

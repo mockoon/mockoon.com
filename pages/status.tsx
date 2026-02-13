82c039721f9db369a09d6281cda52238653d4a67
Guillaume <guillaume@mockoon.com>
2026-02-13 09:38:27 +0100

    Update status

--- pages/status.tsx
+++ pages/status.tsx
@@ -17,8 +17,8 @@ const incidents: {
     title: 'All services - Scheduled downtime',
     desc: 'All services will be restarted for a routine maintenance. The downtime is expected to last less than 5 minutes for each service. We apologize for the inconvenience.',
     dateStart: '2026-02-13T08:00:00Z',
-    dateEnd: '2026-02-13T09:00:00Z',
-    status: 'inprogress'
+    dateEnd: '2026-02-13T08:30:00Z',
+    status: 'resolved'
   },
   {
     title: 'Deployment - US instances - Routine update',

e453b1b000ae505cfe0cfd7f9c6383e4f30dc0d5
Guillaume <guillaume@mockoon.com>
2026-06-19 11:21:39 +0200

    Update status

--- pages/status.tsx
+++ pages/status.tsx
@@ -14,6 +14,13 @@ const incidents: {
   status: 'planned' | 'inprogress' | 'resolved';
 }[] = [
   {
+    title: 'All services - Scheduled downtime for v9.7.0 release',
+    desc: 'All services will be restarted for the deployment of v9.7.0. The downtime is expected to last less than 15 minutes for each service.<br>Update (09:20 UTC): Update of the sync server has started.<br>Update (09:25 UTC): Update of the sync server is finished.<br>Update (09:30 UTC): Update of the eu-1 instance has started.',
+    dateStart: '2026-06-19T09:15:00Z',
+    dateEnd: '2026-04-01T08:22:00Z',
+    status: 'inprogress'
+  },
+  {
     title: 'AI assistant - Short outage following an update',
     desc: 'We deployed an update to our AI assistant that caused a short outage. The issue was quickly identified and resolved. We apologize for the inconvenience.',
     dateStart: '2026-05-26T17:30:00Z',
